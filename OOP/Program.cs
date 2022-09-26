671bfa5a39790d7a66a4dea08b29b2c3d53e15f2
Christoffer Ottestig <otte365@hotmail.com>
2022-09-26 19:10:06 +0200

    Extrauppgift volym

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -19,15 +19,24 @@
                 float area = _Radius * _Radius * _pi;
                 return area;
             }
+
+            public float GetVolume()
+            {
+                double volumeDouble = 4.187 * Math.Pow(_Radius, 3);
+                float volumeFloat = (float)volumeDouble;
+                return volumeFloat;
+            }
         }
 
         static void Main(string[] args)
         {
             Circle circle1 = new Circle(5);
             Circle circle2 = new Circle(6);
+            Circle sphere1 = new Circle(8);
 
             Console.WriteLine("Den första cirkelns radie är: " + circle1.GetArea());
             Console.WriteLine("Den andra cirkelns radie är: " + circle2.GetArea());
+            Console.WriteLine("Sfärens volym är: " + sphere1.GetVolume());
         }
     }
 }
