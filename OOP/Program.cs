0115b2d3e878a34692543610d65d0151821bfef4
Christoffer Ottestig <otte365@hotmail.com>
2022-09-26 12:03:30 +0200

    Grunduppgiften klar!

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -2,9 +2,32 @@
 {
     internal class Program
     {
+        class Circle
+        {
+            float _pi;
+            int _Radius;
+
+            public Circle(int radius)
+            {
+                _Radius = radius;
+                _pi = 3.14f;
+            }
+
+            public float GetArea()
+            {
+
+                float area = _Radius * _Radius * _pi;
+                return area;
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Testar!");
+            Circle circle1 = new Circle(5);
+            Circle circle2 = new Circle(6);
+
+            Console.WriteLine("Den första cirkelns radie är: " + circle1.GetArea());
+            Console.WriteLine("Den andra cirkelns radie är: " + circle2.GetArea());
         }
     }
 }
