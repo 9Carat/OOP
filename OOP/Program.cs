135c13fb96d58a81922b0fe75129942024f1130e
Christoffer Ottestig <otte365@hotmail.com>
2022-09-27 11:49:48 +0200

    Flyttade klasserna inuti Programklassen

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,51 +1,50 @@
 ﻿namespace OOP
 {
-    class Triangle
-    {
-        float _Base;
-        float _Height;
-
-        public Triangle(float Base, float Height )
-        {
-            _Base = Base;
-            _Height = Height;
-        }
-
-        public float GetArea()
-        {
-            float area = _Base * _Height / 2;
-            return area;
-        }
-    }
-
-
-    class Circle
-    {
-        float _pi;
-        int _Radius;
-
-        public Circle(int radius)
-        {
-            _Radius = radius;
-            _pi = 3.14f;
-        }
-
-        public float GetArea()
-        {
-            float area = _Radius * _Radius * _pi;
-            return area;
-        }
-
-        public float GetVolume()
-        {
-            double volumeDouble = 4.187 * Math.Pow(_Radius, 3);
-            float volumeFloat = (float)volumeDouble;
-            return volumeFloat;
-        }
-    }
-
     internal class Program
     {
+        class Triangle
+        {
+            float _Base;
+            float _Height;
+
+            public Triangle(float Base, float Height)
+            {
+                _Base = Base;
+                _Height = Height;
+            }
+
+            public float GetArea()
+            {
+                float area = _Base * _Height / 2;
+                return area;
+            }
+        }
+
+        class Circle
+        {
+            float _pi;
+            int _Radius;
+
+            public Circle(int radius)
+            {
+                _Radius = radius;
+                _pi = 3.14f;
+            }
+
+            public float GetArea()
+            {
+                float area = _Radius * _Radius * _pi;
+                return area;
+            }
+
+            public float GetVolume()
+            {
+                double volumeDouble = 4.187 * Math.Pow(_Radius, 3);
+                float volumeFloat = (float)volumeDouble;
+                return volumeFloat;
+            }
+        }
+
         static void Main(string[] args)
         {
             Circle circle1 = new Circle(5);
