fc92d062049a288f6e29e60d901ff67fe7b6f02c
Christoffer Ottestig <otte365@hotmail.com>
2022-10-04 21:32:25 +0200

    Lade till metod för triangelomkrets samt några kommentarer.

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -4,42 +4,58 @@
     {
         class Triangle
         {
-            float _Base;
-            float _Height;
+            float Base;
+            float Height;
+            float Side1;
+            float Side2;
+            float Side3;
 
-            public Triangle(float Base, float Height)
+            public Triangle(float Side1, float Side2, float Side3) // Constructor for calculation of circumference
             {
-                _Base = Base;
-                _Height = Height;
+                this.Side1 = Side1;
+                this.Side2 = Side2;
+                this.Side3 = Side3;
             }
 
-            public float GetArea()
+            public Triangle(float Base, float Height) // Constructor for calculation of area
             {
-                float area = _Base * _Height / 2;
+                this.Base = Base;
+                this.Height = Height;
+            }
+
+            public float GetArea() // Methods for area and circumference
+            {
+                float area = Base * Height / 2;
                 return area;
+            }
+
+            public float GetCircumference()
+            {
+                float circ = Side1 + Side2 + Side3;
+                return circ;
             }
         }
 
         class Circle
         {
-            float _pi;
-            int _Radius;
+            float pi;
+            int Radius;
 
             public Circle(int radius)
             {
-                _Radius = radius;
-                _pi = 3.14f;
+                this.Radius = radius;
+                this.pi = 3.14f;
             }
 
             public float GetArea()
             {
-                float area = _Radius * _Radius * _pi;
+                float area = Radius * Radius * pi;
                 return area;
             }
 
             public float GetVolume()
             {
-                double volumeDouble = 4.187 * Math.Pow(_Radius, 3);
+                double volumeDouble = 4.187 * Math.Pow(Radius, 3);
                 float volumeFloat = (float)volumeDouble;
                 return volumeFloat;
             }
@@ -51,11 +67,13 @@
             Circle circle2 = new Circle(6);
             Circle sphere1 = new Circle(8);
             Triangle triangle1 = new Triangle(4, 9);
+            Triangle triangle2 = new Triangle(5, 12, 5);
 
             Console.WriteLine("Den första cirkelns area är: " + circle1.GetArea());
             Console.WriteLine("Den andra cirkelns area är: " + circle2.GetArea());
             Console.WriteLine("Sfärens volym är: " + sphere1.GetVolume());
-            Console.WriteLine("Triangelns area är: " + triangle1.GetArea());
+            Console.WriteLine("Den första triangelns area är: " + triangle1.GetArea());
+            Console.WriteLine("Den andra triangelns omkrets är: " + triangle2.GetCircumference());
         }
     }
 }
